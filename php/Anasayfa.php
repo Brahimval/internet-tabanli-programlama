5b7dde835d01e3d74f33a5892c8dfd4b11339bb9
Brahimval <66087046+Brahimval@users.noreply.github.com>
2021-06-21 23:01:41 +0300

    Anasayfa.php dosyasi Güncellendi

---
+++ php/Anasayfa.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ana Sayfa</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="../css/Ana-sayfa.css">
+    </head>
+    <body>
+        <div class="headcard">
+        <div class="sagcubuk">
+            <a href="#P1" >ANA SAYFA</a>
+            <a href="">KATEGORILER</a>
+            <a href="#P5">SORUN BILDIR</a>
+            <a href="#P6">BIZE ULAŞIN</a>
+        </div>
+        <header class="" style="max-width: 1500px;" id="P1" >
+            <img class="foto" src="../images/linkedin-sales-solutions-Be5aVKFv9ho-unsplash.jpg" width="1500" 
+            height="800" alt="">
+
+        
+
+    </div>
+    <div class="mainCards">
+
+        <div class="cardContent">
+          <i> 
+            <img src="../images/erkek1_icon.png" alt="">
+          </i>
+
+            <h3>Mert Sert</h3>
+            <h5>Danışman Bilgileri :</h5>
+          <p>Default text Fairy Tales are stories involving 
+            fantastic forces, usually good versus evil, most
+             originating in folklore, mythology, and legend.
+              They feature fantasy beings like dragons, dwarfs,
+               fairies and talking animals. They aren't just for kids.
+                In fact, many are complex morality tales; some are graphi
+                c or morbid, and shouldn't be read to children at all. 
+                We recommend previewing stories before reading with your children.</p>
+
+
+
+        </div>
+
+    <div class="cardContent">
+        <i> 
+          <img src="../images/icon.png" alt="">
+        </i>
+      
+          
+            <h3>Ahmet Yildiz</h3>
+            <h5>Danışman Bilgileri :</h5>
+          <p>Default text Fairy Tales are stories involving 
+            fantastic forces, usually good versus evil, most
+             originating in folklore, mythology, and legend.
+              They feature fantasy beings like dragons, dwarfs,
+               fairies and talking animals. They aren't just for kids.
+                In fact, many are complex morality tales; some are graphi
+                c or morbid, and shouldn't be read to children at all. 
+                We recommend previewing stories before reading with your children.</p>
+        </div>
+
+
+
+        <div class="cardContent">
+          <i> 
+            <img src="../images/kiz2_icon.png" alt="">
+          </i>
+
+            <h3>Ayşe Gül</h3>
+            <h5>Danışman Bilgileri :</h5>
+
+          <p>Default text Fairy Tales are stories involving 
+            fantastic forces, usually good versus evil, most
+             originating in folklore, mythology, and legend.
+              They feature fantasy beings like dragons, dwarfs,
+               fairies and talking animals. They aren't just for kids.
+                In fact, many are complex morality tales; some are graphi
+                c or morbid, and shouldn't be read to children at all. 
+                We recommend previewing stories before reading with your children.</p>
+
+
+        </div>
+
+       
+        </div>
+
+        <div class="mainCards2">
+
+
+
+
+          <div class="cardContent">
+            <i> 
+              <img src="../images/kiz3_icon.png" alt="">
+            </i>
+
+            <h3>Hatice Ahmet</h3>
+            <h5>Danışman Bilgileri :</h5>
+          <p>Default text Fairy Tales are stories involving 
+            fantastic forces, usually good versus evil, most
+             originating in folklore, mythology, and legend.
+              They feature fantasy beings like dragons, dwarfs,
+               fairies and talking animals. They aren't just for kids.
+                In fact, many are complex morality tales; some are graphi
+                c or morbid, and shouldn't be read to children at all. 
+                We recommend previewing stories before reading with your children.</p>
+
+
+
+
+
+        </div>
+
+        <div class="cardContent">
+
+
+          <i> 
+            <img src="../images/erkek1_icon.png" alt="">
+          </i>
+
+
+          <h3>Zülküf Ali</h3>
+          <h5>Danışman Bilgileri :</h5>
+        <p>Default text Fairy Tales are stories involving 
+          fantastic forces, usually good versus evil, most
+           originating in folklore, mythology, and legend.
+            They feature fantasy beings like dragons, dwarfs,
+             fairies and talking animals. They aren't just for kids.
+              In fact, many are complex morality tales; some are graphi
+              c or morbid, and shouldn't be read to children at all. 
+              We recommend previewing stories before reading with your children.</p>
+
+
+
+
+      </div>
+        
+
+      <div class="cardContent">
+
+
+        <i> 
+          <img src="../images/erkek1_icon.png" alt="">
+        </i> 
+
+        <h3>Osman Yıldız</h3>
+        <h5>Danışman Bilgileri :</h5>
+      <p>Default text Fairy Tales are stories involving 
+        fantastic forces, usually good versus evil, most
+         originating in folklore, mythology, and legend.
+          They feature fantasy beings like dragons, dwarfs,
+           fairies and talking animals. They aren't just for kids.
+            In fact, many are complex morality tales; some are graphi
+            c or morbid, and shouldn't be read to children at all. 
+            We recommend previewing stories before reading with your children.</p>
+
+
+
+    </div>
+
+        </div>
+
+        </div>
+
+
+
+     <div class="sorunBildir" id="P5">
+     <input type="text" placeholder="Bildirmek istediğinizi yazınız....... ">
+     <br>
+     <br>
+     <button>Sorunu Bildir</button>
+     </div>
+
+
+   <div class="footer" id="P6">
+     <div class="footerContent">
+       <ul>
+
+         <h3>Bize ulaşın</h3>
+         <li> <p>xxxxxxx@gmail.com</p></li> 
+         <li> <p>fix :212 023 232 32 24 </p></li>
+         <li> <p>Phone  :212 023 232 32 25 </p></li>
+         <li><p>Adres : XXXXX Mahallesi XXXXX cadesi No:11 istanbul/Turkiye</p></li> 
+      </ul>
+     </div>
+   </div>
+        
+    </body>
+</html>
